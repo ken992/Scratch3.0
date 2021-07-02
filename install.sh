0db534b020a4c6bf5f968a77a5621de7b27b767a
ken992 <fire09682@gmail.com>
2021-07-02 22:39:42 +0900

    初回コンテナ起動時のみinstallする

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+if [ -f /usr/src/app/completed ]; then
+  echo "Installation skipped"
+  tail -f  /dev/null
+fi
 cp -p /home/node/package.json /usr/src/app/.
 cd /usr/src/app
 git clone --depth 1 https://github.com/llk/scratch-gui.git
@@ -10,6 +13,5 @@ yarn link
 cd ../scratch-gui
 yarn link scratch-vm
 yarn install
-
+touch /usr/src/app/completed
 tail -f  /dev/null 
-
